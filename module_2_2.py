4ece3a89de1979dd35d13c290042d81db9ebf423
Helga <helga1538@mail.ru>
2024-09-24 14:47:31 +0300

    Update module_2_2.py
    Преобразованы данные из str в int (9-11)

--- module_2_2.py
+++ module_2_2.py
@@ -6,9 +6,9 @@
 # Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
 # Если равных чисел среди 3-х вообще нет, то вывести 0
 
-first = input('Введите первое число')
-second = input('Введите второе число')
-third = input('Введите третье число')
+first = int(input('Введите первое число'))
+second = int(input('Введите второе число'))
+third = int(input('Введите третье число'))
 if first == second and first == third and second == third:
     print(3)
 elif first == second or first == third or second == third:
